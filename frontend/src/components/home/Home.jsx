ef5754338ac920853587546176b51dfef3c0efd2
JoaoVGStahl <jvitorstahl@hotmail.com>
2021-10-18 20:36:48 -0300

    sintaxe fix.

--- frontend/src/components/home/Home.jsx
+++ frontend/src/components/home/Home.jsx
@@ -1,10 +1,10 @@
 import React from 'react'
 import Main from '../template/Main'
 
-export default props=>
+export default props =>
     <Main icon="home" title="Início"
         subtitle="Controle de Despesas">
-            <div className="display-4">Bem Vindo!</div>
-            <hr/>
-            <p className="mb-0">Cuide da suas finanças com segurança!</p>
-        </Main>
+        <div className="display-4">Bem Vindo!</div>
+        <hr />
+        <p className="mb-0">Cuide da suas finanças com segurança!</p>
+    </Main>
--- frontend/src/components/receitas/ReceitasCrud.jsx
+++ frontend/src/components/receitas/ReceitasCrud.jsx
@@ -3,94 +3,89 @@ import { Component } from 'react'
 import axios from 'axios'
 import Main from '../template/Main'
 
-const headerProps ={
-    icon : 'money',
-    title : 'Receitas',
-    subtitle : 'Gerenciamento de Receitas!'
+const headerProps = {
+    icon: 'money',
+    title: 'Receitas',
+    subtitle: 'Gerenciamento de Receitas!'
 }
 const baseUrl = 'http://localhost:3001/receitas'
-const initialState ={
-    receitas : {descricao : '', valor : ''},
-    list : []
+const initialState = {
+    receitas: { descricao: '', valor: '' },
+    list: []
 }
-export default class Receitas extends Component{
-    state = {...initialState}
+export default class Receitas extends Component {
+    state = { ...initialState }
 
-    componentWillMount(){
-        axios(baseUrl).then(resp =>{
-            this.setState({list : resp.data})
+    componentWillMount() {
+        axios(baseUrl).then(resp => {
+            this.setState({ list: resp.data })
         })
     }
-    clear(){
-        this.setState({receitas: initialState.receitas})
+    clear() {
+        this.setState({ receitas: initialState.receitas })
     }
-    remove(receitas){
+    remove(receitas) {
         axios.delete(`${baseUrl}/${receitas.id}`)
-            .then(resp =>{
+            .then(resp => {
                 const list = this.getUpdatedList(receitas, false)
-                this.setState({list})
+                this.setState({ list })
             })
     }
-    getUpdatedList(receitas, add = true){
+    getUpdatedList(receitas, add = true) {
         const list = this.state.list.filter(u => u.id !== receitas.id)
-        if(add)
+        if (add)
             list.unshift(receitas)
         return list
     }
-    updateField(event){
-        const receitas = {...this.state.receitas}
+    updateField(event) {
+        const receitas = { ...this.state.receitas }
         receitas[event.target.name] = event.target.value
-        this.setState({receitas})
+        this.setState({ receitas })
     }
-    load(receitas){
-        this.setState({receitas})
+    load(receitas) {
+        this.setState({ receitas })
     }
-    save(){
+    save() {
         const receitas = this.state.receitas
         const method = receitas.id ? 'put' : 'post'
         const url = receitas.id ? `${baseUrl}/${receitas.id}` : baseUrl
 
         axios[method](url, receitas)
-            .then(resp =>{
+            .then(resp => {
                 const list = this.getUpdatedList(resp.data)
-                this.setState({receitas : initialState.receitas, list})
+                this.setState({ receitas: initialState.receitas, list })
             })
     }
-    renderForm(){
-        return(
+    renderForm() {
+        return (
             <div className="form">
                 <div className="row">
                     <div className="col-12 col-md-6">
                         <div className="form-group">
                             <label>Origem da Receita:</label>
-                            <input type="text" className="form-control" 
-                            name="descricao" 
-                            value={this.state.receitas.descricao}
-                            onChange={e => this.updateField(e)}
-                            placeholder="Digite a origem da despesa"/>
+                            <input type="text" className="form-control"
+                                name="descricao"
+                                value={this.state.receitas.descricao}
+                                onChange={e => this.updateField(e)}
+                                placeholder="Digite a origem da despesa" />
                         </div>
                     </div>
                     <div className="col-12 col-md-6">
                         <label>Valor:</label>
-                        <input type="text" className="form-control" 
-                        name="valor" 
-                        value={this.state.receitas.valor}
-                        onChange={e => this.updateField(e)}
-                        placeholder="Digite o Valor da receita..."
-                        />
+                        <input type="text" className="form-control"
+                            name="valor"
+                            value={this.state.receitas.valor}
+                            onChange={e => this.updateField(e)}
+                            placeholder="Digite o Valor da receita..." />
                     </div>
-                    <div className="row">
-                    <div className="col-12 d-flex justify-content-end">
-                        <button className="btn btn-primary"
-                            onClick={e => this.save(e)}>
+                    <div className="col-12 d-flex justify-content-start">
+                        <button className="btn btn-primary" onClick={e => this.save(e)}>
                             Salvar
                         </button>
-                        <button className="btn btn-secondary ml-2"
-                            onClick={e => this.clear(e)}>
+                        <button className="btn btn-secondary ml-2" onClick={e => this.clear(e)}>
                             Cancelar
                         </button>
                     </div>
-                </div>
                 </div>
             </div>
         )
@@ -102,8 +97,8 @@ export default class Receitas extends Component{
                     <thead>
                         <tr>
                             <th>#</th>
-                            <th>Descricao</th>
                             <th>Origem</th>
+                            <th>Valor</th>
                             <th>Ações</th>
                         </tr>
                     </thead>
@@ -115,20 +110,19 @@ export default class Receitas extends Component{
         )
     }
     renderRows() {
-        return this.state.list.map(receitas =>{
-            console.log("receitas:" + receitas)
-            return(
-                
+        return this.state.list.map(receitas => {
+            return (
+
                 <tr key={receitas.id}>
                     <td>{receitas.id}</td>
                     <td>{receitas.descricao}</td>
-                    <td>{receitas.valor}</td>  
+                    <td>{receitas.valor}</td>
                     <td>
                         <button className="btn btn-warning"
-                            onClick={() => this.Load(receitas)}>
+                            onClick={() => this.load(receitas)}>
                             <i className="fa fa-pencil"></i>
                         </button>
-                        <button className="btn btn-danger ml-2" 
+                        <button className="btn btn-danger ml-2"
                             onClick={() => this.remove(receitas)}>
                             <i className="fa fa-trash"></i>
                         </button>
